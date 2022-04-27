6d9b1724822b74f897c757a7c9606f03252e1f9c
Andre Nepomuceno <andre.nepa@gmail.com>
2022-04-27 19:18:15 -0300

    Initial Commit

---
+++ DesafioCustomViewTableCell2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesafioCustomViewTableCell2
+//
+//  Created by André N. dos Santos on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
